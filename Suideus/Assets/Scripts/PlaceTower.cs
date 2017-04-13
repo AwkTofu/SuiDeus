5609e9a21479a91e9c2a89a82dda21fd4a8f2c27
FanZhong <fanzhongzeng78@gmail.com>
2017-04-13 12:02:02 -0400

    Added Project, Only have Spawning Towers working

---
+++ Suideus/Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlaceTower : MonoBehaviour {
+
+	public GameObject towerPrefab;
+	private GameObject tower;
+    private GameManagerBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+    }
+
+    private bool canPlaceTower() 
+	{
+        int cost = towerPrefab.GetComponent<TowerData>().levels[0].cost;
+        return tower == null && gameManager.Gold >= cost;
+    }
+
+	void OnMouseUp () 
+	{
+		if (canPlaceTower()) 
+		{
+		  	tower = (GameObject) 
+		    Instantiate(towerPrefab, transform.position, Quaternion.identity);
+
+            /* This is how audio works
+		    AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+    		audioSource.PlayOneShot(audioSource.clip);
+    		*/
+
+            //Deduct gold
+            gameManager.Gold -= tower.GetComponent<TowerData>().CurrentLevel.cost;
+        }
+        else if (canUpgradeTower())
+        {
+            tower.GetComponent<TowerData>().increaseLevel();
+            //Deduct gold
+            gameManager.Gold -= tower.GetComponent<TowerData>().CurrentLevel.cost;
+        }
+    }
+
+    private bool canUpgradeTower()
+    {
+        if (tower != null)
+        {
+            TowerData towerData = tower.GetComponent<TowerData>();
+            TowerLevel nextLevel = towerData.getNextLevel();
+            if (nextLevel != null)
+            {
+                return gameManager.Gold >= nextLevel.cost;
+            }
+        }
+        return false;
+    }
+}
---
+++ Suideus/Assets/Scripts/TowerData.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+/*
+You add[System.Serializable] at the top to make instances of the class editable from the inspector.
+This allows you to quickly change all values in the Level class — even while the game is running.
+It’s incredibly useful for balancing your game.
+*/
+
+public class TowerLevel
+{
+    public int cost;
+    public GameObject visualization;
+}
+
+public class TowerData : MonoBehaviour {
+
+    public List<TowerLevel> levels;
+    private TowerLevel currentLevel;
+
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+
+    public TowerLevel CurrentLevel
+    {        
+        get
+        {
+            return currentLevel;
+        }        
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    public TowerLevel getNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void increaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+}
+
+
